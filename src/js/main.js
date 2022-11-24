d4e3bd81a34281d5330bf0e43f5da2652c61d658
Daniel Schreurs <daniel.schreurs@hotmail.com>
2022-11-24 09:08:45 +0100

    :tada: initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,39 @@
 (
     function () {
-      const Tabs = {
-        init() {
-          this.cacheDom();
-          this.enableJavaScript();
-          this.setupTabs();
-        },
-        cacheDom() {
-          this.tabLinks = Array.from(document.querySelectorAll('.tablinks'));
-          this.tabContent = Array.from(document.querySelectorAll('.tabcontent'));
-          this.HtmlElement = window.document.documentElement;
-        },
-        enableJavaScript() {
-          this.HtmlElement.classList.add('js-enabled');
-        },
-        setupTabs() {
-          for (const link of this.tabLinks) {
-            link.addEventListener('click', el => {
-              this.openTabs(el);
-            });
-          }
-        },
-        openTabs(el) {
-          const btnTarget = el.currentTarget;
-          const { country } = btnTarget.dataset;
-          this.tabLinks.forEach(link => {
-            link.classList.remove('active');
-          });
-          this.tabContent.forEach(tab => {
-            tab.classList.remove('active');
-          });
-          document.querySelector(`#${country}`).classList.add('active');
-          btnTarget.classList.add('active');
-        },
-      };
-      Tabs.init();
+        const Tabs = {
+            init() {
+                this.cacheDom();
+                this.enableJavaScript();
+                this.setupTabs();
+            },
+            cacheDom() {
+                this.tabLinks = document.querySelectorAll('.tablinks');
+                this.tabContent = document.querySelectorAll('.tabcontent');
+                this.HtmlElement = window.document.documentElement;
+            },
+            enableJavaScript() {
+                this.HtmlElement.classList.add('js-enabled');
+            },
+            setupTabs() {
+                this.tabLinks.forEach(link => {
+                    link.addEventListener('click', el => {
+                        this.openTabs(el);
+                    });
+                });
+            },
+            openTabs(el) {
+                const btnTarget = el.currentTarget;
+                const {country} = btnTarget.dataset;
+                this.tabLinks.forEach(link => {
+                    link.classList.remove('active');
+                });
+                this.tabContent.forEach(tab => {
+                    tab.classList.remove('active');
+                });
+                document.querySelector(`#${country}`).classList.add('active');
+                btnTarget.classList.add('active');
+            },
+        };
+        Tabs.init();
     }
 )();
